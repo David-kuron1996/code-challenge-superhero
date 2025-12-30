1c0a97e0263c56a0dc861fce0b3234011d24d102
David <davidkuron1996@gmail.com>
2025-12-30 21:27:24 +0300

    add this file

--- models.py
+++ models.py
@@ -27,3 +27,24 @@ class Power(db.Model):
             "name": self.name,
             "description": self.description
         }
+    
+class HeroPower(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    strength = db.Column(db.String, nullable=False)
+    hero_id = db.Column(db.Integer, db.ForeignKey('hero.id'))
+    power_id = db.Column(db.Integer, db.ForeignKey('power.id'))
+
+    VALID_STRENGTHS = ['Strong', 'Weak', 'Average']
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "hero_id": self.hero_id,
+            "power_id": self.power_id,
+            "strength": self.strength,
+            "hero": self.hero.to_dict(),
+            "power": self.power.to_dict()
+        }
+
+    def is_valid(self):
+        return self.strength in self.VALID_STRENGTHS
