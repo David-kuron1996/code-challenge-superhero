3e8a78ec7485c0499df5b994c007a8b5dc6d1a3f
David <davidkuron1996@gmail.com>
2025-12-30 21:18:32 +0300

    add this file

--- app.py
+++ app.py
@@ -8,3 +8,23 @@ app.config.from_object(Config)
 
 db.init_app(app)
 migrate = Migrate(app, db)
+
+# GET all heroes
+@app.route('/heroes', methods=['GET'])
+def get_heroes():
+    heroes = Hero.query.all()
+    return jsonify([h.to_dict() for h in heroes]), 200
+
+# GET all powers
+@app.route('/powers', methods=['GET'])
+def get_powers():
+    powers = Power.query.all()
+    return jsonify([p.to_dict() for p in powers]), 200
+
+# POST hero power
+@app.route('/hero_powers', methods=['POST'])
+def create_hero_power():
+    data = request.get_json()
+    hero_id = data.get("hero_id")
+    power_id = data.get("power_id")
+    strength = data.get("strength")
