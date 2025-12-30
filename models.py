820cc63cc25f51fae9fb835ea4325235148c5a3c
David <davidkuron1996@gmail.com>
2025-12-30 21:25:19 +0300

    add this file

---
+++ models.py
@@ -0,0 +1,29 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Hero(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    super_name = db.Column(db.String, nullable=False)
+    hero_powers = db.relationship('HeroPower', backref='hero', cascade="all, delete")
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "super_name": self.super_name
+        }
+
+class Power(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    description = db.Column(db.String, nullable=False)
+    hero_powers = db.relationship('HeroPower', backref='power', cascade="all, delete")
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description
+        }
